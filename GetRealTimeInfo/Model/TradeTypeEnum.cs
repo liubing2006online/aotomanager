0dec3357243f054aca0b34f860222d97b4baf71a
zichen8 <zichen84@gmail.com>
2017-10-09 09:49:22 +0800

    add enum

---
+++ GetRealTimeInfo/Model/TradeTypeEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetRealTimeInfo.Model
+{
+    public enum TradeTypeEnum : int
+    {
+        /// <summary>
+        /// 买
+        /// </summary>
+        [Description("买")]
+        Buy = 1,
+        /// <summary>
+        /// 卖
+        /// </summary>
+        [Description("卖")]
+        Sale = 2
+    }
+}
